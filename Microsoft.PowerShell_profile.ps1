26ed2407cd437857d8e61527b99b4223915f6a2c
YuSuFjel <yousseffjelit@gmail.com>
2024-03-31 22:45:18 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -206,4 +206,4 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 ## Final Line to set prompt
 # oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json | Invoke-Expression
-oh-my-posh init pwsh --config 'C:\Users\YuSuf\Documents\Powershell\themes\amro.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\YuSuf\Documents\Github\Powershell\themes\amro.omp.json' | Invoke-Expression
